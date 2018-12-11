f4dd380e1b7bf954d5897e314746159f0fe6a907
Péter Finta <finta96peter@gmail.com>
2018-12-11 14:13:55 -0500

    New style and sample audio, removed mic feature

--- shatter.js
+++ shatter.js
@@ -1,14 +1,15 @@
-var song, mic, fft;
+var song, fft;
+
+function preload(){
+    song = loadSound('off.mp3');
+}
 
 function setup() {
    createCanvas(512,512);
    noFill();
-   song = loadSound('zgr.mp3');
-  
-   mic = new p5.AudioIn();
-   mic.start();
-   fft = new p5.FFT(0.3);
-   fft.setInput(mic);
+   song.loop();
+   fft = new p5.FFT();
+   fft.setInput(song);
 }
 
 function draw() {
@@ -18,9 +19,13 @@ function draw() {
      var unit = height/4;
      var cx = width/2;
      var cy= height/2;
-  from = color(0, 0, 0, 255);
-  to = color(127, 0, 255, 255);
+   var cov = 0.9;
+    var cov2 =1.1;
+  from = color(0, 0, 32, 32);
+  to = color(255, 0, 255, 255);
+  var invis = color (127,0,127,0);
   var step = 3.1415/4;
+  var scv=9.05;
   stroke(0);
   beginShape();
   vertex(0+cx,0+cy);
@@ -31,34 +36,45 @@ function draw() {
   // for (i = 0; i<100; i++) {
 //     var mag= map(spectrum[i], 0, 255, 0, 1);
      var prog = i / spectrum.length;
-     var oct = log(i,2)*9.0;
+     var oct = log(i,2)*scv;
      var octv = oct/(2*3.1415);
-     var octmax = log(spectrum.length,2)*9.0;
+     var octmax = log(spectrum.length,2)*scv;
      var octvmax = octmax/(2*3.1415);
-     
      var x = sin(oct);
      var y = cos(oct);
-     var oct2 = log(i+1,2)*9.0;
+     var oct2 = log(i+cov,2)*scv;
      var x2 = sin(oct2);
      var y2 = cos(oct2);
+     var oct3 = log(i+cov*0.5,2)*scv;
+     var x3 = sin(oct3);
+     var y3 = cos(oct3);
      var sw = unit/8;
      var bmax=(octvmax);
      var ratio = 2*unit/bmax;
-     
      var b1=(octv)*ratio;
-     var b2=(octv-1)*ratio;
+     var b2=(octv-cov2)*ratio;
+     var b3=(octv-0.1)*ratio;
+     var b4=(octv-0.1-cov2)*ratio;
      var spec = map(spectrum[i], 0, 255, 0, 1);
      var mag= map(spec, 0, 1, b1, b2);
      
-      c = lerpColor(from, to, spec);
-      stroke(c);
+      c = lerpColor(from, to, spec*1);
+      noStroke();
      fill(c);
      beginShape();
     vertex(b1*x+width/2, b1*y+height/2);
     vertex(b2*x+width/2, b2*y+height/2);
     vertex(b2*x2+width/2, b2*y2+height/2);
     vertex(b1*x2+width/2, b1*y2+height/2);
+    vertex(b1*x3+width/2, b1*y3+height/2);
      endShape();
+    beginShape();
+    vertex(b3*x+width/2, b3*y+height/2);
+    vertex(b4*x+width/2, b4*y+height/2);
+    vertex(b4*x2+width/2, b4*y2+height/2);
+    vertex(b3*x2+width/2, b3*y2+height/2);
+    vertex(b3*x3+width/2, b3*y3+height/2);
+    endShape();
    }
   // endShape();
 }
@@ -67,13 +83,9 @@ function mousePressed() {
   if ( song.isPlaying() ) { // .isPlaying() returns a boolean
     song.pause(); // .play() will resume from .pause() position
     // background(255,0,0);
-   fft = new p5.FFT();
-   fft.setInput(mic);
   } else {
     song.play();
     // background(0,255,0);
-   fft = new p5.FFT();
-   fft.setInput(song);
   }
 }
 
